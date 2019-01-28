1c7ae02af2e3838d0928c134a4a6fef9ec570e2b
kimukou1205 <39507751+kimukou1205@users.noreply.github.com>
2019-01-28 16:19:01 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -21,7 +21,7 @@ import com.example.axu1.richarddawkinsalarmclock.util.PreferenceUtil;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements AccelerometerListener {
     public static final String ALARM_TIME = "alarm_time";
 
     AlarmManager alarmManager;
@@ -30,14 +30,80 @@ public class MainActivity extends AppCompatActivity  {
     private TimePicker alarmTimePicker;
     private TextView alarmTextView;
     private TextView countTextView;
-    Context context;
 
     private AlarmReceiver alarm;
 
+    private Integer shakeCount;
 
 
     MainActivity inst;
+    Context context;
     PreferenceUtil pref;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (AccelerometerManager.isSupported(this)) {
+            AccelerometerManager.startListening(this);
+        }
+    }
+
+    @Override
+    public void onAccelerationChanged(float x, float y, float z) {
+
+    }
+
+    @Override
+    public void onShake(float force) {
+        //stopAlarm();
+        shakeCount++;
+        //countTextView = (TextView) findViewById(R.id.txtShakeCount);
+        Toast.makeText(this, shakeCount.toString() + "回", Toast.LENGTH_SHORT).show();
+
+
+        if (shakeCount >= 10) {
+
+            //------------------
+            this.context = this;
+            final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
+
+            int min = 1;
+            int max = 9;
+
+            Random r = new Random();
+            int random_number = r.nextInt(max - min + 1) + min;
+            Log.e("random number is ", String.valueOf(random_number));
+
+            myIntent.putExtra("extra", "no");
+            sendBroadcast(myIntent);
+
+            alarmManager.cancel(pending_intent);
+            setAlarmText("アラーム終了しました");
+            //setAlarmText("You clicked a " + " canceled");
+            shakeCount = 0;
+        }
+
+
+        //countTextView.setText(shakeCount.toString());
+
+
+        //------------------
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+//Check device supported Accelerometer senssor or not
+        if (AccelerometerManager.isListening()) {
+
+//Start Accelerometer Listening
+            AccelerometerManager.stopListening();
+
+            //Toast.makeText(this, "onStop Accelerometer Stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
     @Override
@@ -52,8 +118,11 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(intent);
             }
         });
+
         this.context = this;
         final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
+        shakeCount = 0;
+
 
         //alarm = new AlarmReceiver();
         alarmTextView = (TextView) findViewById(R.id.alarmText);
@@ -75,7 +144,7 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
 
-                calendar.add(Calendar.SECOND, 3);
+                calendar.add(Calendar.SECOND, 2);
                 //setAlarmText("You clicked a button");
 
                 final int hour = alarmTimePicker.getCurrentHour();
@@ -104,9 +173,84 @@ public class MainActivity extends AppCompatActivity  {
         });
 
 
+        Button stop_alarm = (Button) findViewById(R.id.stop_alarm);
+        stop_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int min = 1;
+                int max = 9;
+
+                Random r = new Random();
+                int random_number = r.nextInt(max - min + 1) + min;
+                Log.e("random number is ", String.valueOf(random_number));
+
+                myIntent.putExtra("extra", "no");
+                sendBroadcast(myIntent);
+
+                alarmManager.cancel(pending_intent);
+                setAlarmText("アラーム終了");
+                //setAlarmText("You clicked a " + " canceled");
+            }
+        });
+
+
     }
+
+    private void stopAlarm() {
+
+    }
+
     public void setAlarmText(String alarmText) {
         alarmTextView.setText(alarmText);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (AccelerometerManager.isListening()) {
+            AccelerometerManager.stopListening();
+
+            //Toast.makeText(this, "onDestroy Accelerometer Stopped", Toast.LENGTH_SHORT).show();
+        }
+
+        Log.e("MyActivity", "on Destroy");
+    }
+
+    // 登録
+    @SuppressLint("ObsoleteSdkInt")
+    private void register(long alarmTimeMillis) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = getPendingIntent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(alarmTimeMillis, null), pendingIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTimeMillis, pendingIntent);
+        } else {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTimeMillis, pendingIntent);
+        }
+        // 保存
+        pref.setLong(ALARM_TIME, alarmTimeMillis);
+    }
+
+    // 解除
+    private void unregister() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(getPendingIntent());
+        pref.delete(ALARM_TIME);
+    }
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(this, com.example.axu1.richarddawkinsalarmclock.AlarmCheckActivity.class);
+        intent.setClass(this, com.example.axu1.richarddawkinsalarmclock.AlarmCheckActivity.class);
+        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
 }
--- PlaySoundActivity.java
+++ PlaySoundActivity.java
@@ -18,37 +18,25 @@ import android.widget.Toast;
 
 import com.example.axu1.richarddawkinsalarmclock.util.PreferenceUtil;
 
+import java.util.Calendar;
 import java.util.Random;
 
-public class PlaySoundActivity extends AppCompatActivity implements AccelerometerListener {
+//import orz.kassy.shakegesture.ShakeGestureManager;
+
+public class PlaySoundActivity extends AppCompatActivity {
     Button stop;
     int randnum;
     TextView textView;
     TextView textView3;
     EditText editText;
     int count = 0;
-    Context context;
-    private Integer shakeCount;
-    AlarmManager alarmManager;
-    private PendingIntent pending_intent;
-    private TextView alarmTextView;
-    MainActivity inst;
+    Context c;
+    AlarmManager am;
+    private static final String TAG = PlaySoundActivity.class.getSimpleName();
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-//Check device supported Accelerometer senssor or not
-        if (AccelerometerManager.isListening()) {
-//Start Accelerometer Listening
-            AccelerometerManager.stopListening();
-            //Toast.makeText(this, "onStop Accelerometer Stopped", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
+     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+        if (event.getAction()==KeyEvent.ACTION_DOWN) {
             switch (event.getKeyCode()) {
                 case KeyEvent.KEYCODE_BACK:
                     // ダイアログ表示など特定の処理を行いたい場合はここに記述
@@ -60,61 +48,15 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (AccelerometerManager.isSupported(this)) {
-            AccelerometerManager.startListening(this);
-        }
-    }
-
-    @Override
-    public void onAccelerationChanged(float x, float y, float z) {
-
-    }
-
-    @Override
-    public void onShake(float force) {
-        //stopAlarm();
-        shakeCount++;
-        //countTextView = (TextView) findViewById(R.id.txtShakeCount);
-        Toast.makeText(this, shakeCount.toString() + "回", Toast.LENGTH_SHORT).show();
-
-
-        if (shakeCount >= 10) {
-
-            //------------------
-            this.context = this;
-            final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
-
-            int min = 1;
-            int max = 9;
-
-            Random r = new Random();
-            int random_number = r.nextInt(max - min + 1) + min;
-            Log.e("random number is ", String.valueOf(random_number));
-
-            myIntent.putExtra("extra", "no");
-            sendBroadcast(myIntent);
-
-            alarmManager.cancel(pending_intent);
-            setAlarmText("アラーム終了しました");
-            //setAlarmText("You clicked a " + " canceled");
-            shakeCount = 0;
-        }
-    }
-
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_sound);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        this.context = this;
-        final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
-        shakeCount = 0;
-        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+
 
         SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
         final int Level = data.getInt("LevelSave", 1); // 第２引数は値がなかったときのデフォルト
@@ -125,59 +67,59 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
         startService(new Intent(this, PlaySoundService.class));
         Random rand = new Random();
         randnum = rand.nextInt(10);
-        //  randnum = Level;
+      //  randnum = Level;
         textView3 = (TextView) findViewById(R.id.textView3);
         textView = (TextView) findViewById(R.id.textView2);
-        editText = (EditText) findViewById(R.id.editText);
+        editText= (EditText) findViewById(R.id.editText);
 
-        if (Level == 1) {
+       if(Level == 1){
             // 低の問題
-            if (randnum == 0) {
+            if(randnum==0){
                 textView.setText("１１×１１");//121
-            } else if (randnum == 1) {
+            }else if(randnum==1){
                 textView.setText("１＋２＋３＋４＋５＋６＋７＋８＋９");//45
-            } else if (randnum == 2) {
+            }else if(randnum==2){
                 textView.setText("３３×３３");//1089
-            } else if (randnum == 3) {
+            }else if(randnum==3){
                 textView.setText("１＋３＋９＋２７＋８１＋２４３");//364
-            } else if (randnum == 4) {
+            }else if(randnum==4) {
                 textView.setText("(－４)－(－５)");//1
-            } else if (randnum == 5) {
+            }else if(randnum==5) {
                 textView.setText("(－３)－(－２)");//-1
-            } else if (randnum == 6) {
+            }else if(randnum==6) {
                 textView.setText("０－(－５)");//5
-            } else if (randnum == 7) {
+            }else if(randnum==7) {
                 textView.setText("５－(＋２)");//3
-            } else if (randnum == 8) {
+            }else if(randnum==8) {
                 textView.setText("(－４)－(＋６)");//-10
-            } else if (randnum == 9) {
+            }else if(randnum==9) {
                 textView.setText("(＋２)－(－５)");//7
-            } else if (randnum == 10) {
+            }else if(randnum==10) {
                 textView.setText("９－(－５)");//14
             }
         }
-        if (Level == 2) {
+        if(Level == 2){
             // 中の問題
-            if (randnum == 0) {
+            if(randnum==0){
                 textView.setText("１１１×１１１");//12321
-            } else if (randnum == 1) {
+            }else if(randnum==1){
                 textView.setText("(１００－１３)×(１００＋１３)");//9831
-            } else if (randnum == 2) {
+            }else if(randnum==2){
                 textView.setText("２×(－５)");//-10
-            } else if (randnum == 3) {
+            }else if(randnum==3) {
                 textView.setText("(－２)×(－５)");//10
-            } else if (randnum == 4) {
-                textView.setText("(－４)÷２");//-2
-            } else if (randnum == 5) {
-                textView.setText("９÷(－３)");//-3
-            } else if (randnum == 6) {
-                textView.setText("－１０÷(－５)");//2
-            } else if (randnum == 7) {
+            }else if(randnum==4) {
+               textView.setText("(－４)÷２");//-2
+            }else if(randnum==5) {
+               textView.setText("９÷(－３)");//-3
+            }else if(randnum==6) {
+            textView.setText("－１０÷(－５)");//2
+            }else if(randnum==7) {
                 textView.setText("(－１２)÷(－４/３)");//9
             }
         }
 
-        if (Level == 3) {
+        if(Level == 3) {
             // 高の問題
             if (randnum == 0) {
                 textView.setText("９９×(８＋２８７－２２)×０");//0
@@ -195,10 +137,9 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
         }
         final Intent intent = new Intent(this, MainActivity.class);
 
-        if (count == 10) {
+        if(count==10){
             stopsound();
         }
-
         stop = (Button) findViewById(R.id.stop);
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -212,6 +153,7 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
                     } else if (randnum == 1 && editText.getText().toString().equals("45")) {
                         stopsound();
                         textView3.setText("正解!!");
+                        onResume();
                         finish();
                     } else if (randnum == 2 && editText.getText().toString().equals("1089")) {
                         stopsound();
@@ -249,7 +191,7 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
                         stopsound();
                         textView3.setText("正解!!");
                         finish();
-                    } else if (editText.getText().toString().equals("")) {
+                    }else if (editText.getText().toString().equals("")) {
                         Toast.makeText(PlaySoundActivity.this, "数字を入力してください", Toast.LENGTH_LONG).show();
                     }
                 }
@@ -287,12 +229,11 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
                         stopsound();
                         textView3.setText("正解!!");
                         finish();
-                    } else if (editText.getText().toString().equals("")) {
+                    }else if (editText.getText().toString().equals("")) {
                         Toast.makeText(PlaySoundActivity.this, "数字を入力してください", Toast.LENGTH_LONG).show();
                     }
 
-                }
-                if (Level == 3) {
+                }if (Level == 3) {
                     // 高の問題
                     if (randnum == 0 && editText.getText().toString().equals("0")) {
                         stopsound();
@@ -317,68 +258,49 @@ public class PlaySoundActivity extends AppCompatActivity implements Acceleromete
                     } else if (randnum == 5 && editText.getText().toString().equals("9")) {
                         stopsound();
                         textView3.setText("正解!!");
-                        finish();
-                    } else if (editText.getText().toString().equals("")) {
+                    }else if (editText.getText().toString().equals("")) {
                         Toast.makeText(PlaySoundActivity.this, "数字を入力してください", Toast.LENGTH_LONG).show();
                     }
                 }
             }
-
-        });
-        Button stop_alarm = (Button) findViewById(R.id.stop_alarm);
-        stop_alarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int min = 1;
-                int max = 9;
-
-                Random r = new Random();
-                int random_number = r.nextInt(max - min + 1) + min;
-                Log.e("random number is ", String.valueOf(random_number));
-
-                myIntent.putExtra("extra", "no");
-                sendBroadcast(myIntent);
-
-                alarmManager.cancel(pending_intent);
-                setAlarmText("アラーム終了");
-                //setAlarmText("You clicked a " + " canceled");
-            }
         });
     }
 
-    public void stopsound() {
+    public void stopsound(){
         stopService(new Intent(PlaySoundActivity.this, PlaySoundService.class));
         PreferenceUtil pref = new PreferenceUtil(PlaySoundActivity.this);
         pref.delete(com.example.axu1.richarddawkinsalarmclock.MainActivity.ALARM_TIME);
-    }
-
-    public void setAlarmText(String alarmText) {
-        alarmTextView.setText(alarmText);
-    }
-
-    private void stopAlarm() {
 
     }
-
 
 
     /*@Override
-    public void onStart() {
-        super.onStart();
-        inst = this;
-    }*/
+    public void finish()
+        {
+            // 初期化
+            this.c = c;
+            am = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+            Log.v(TAG, "初期化完了");
+        }*/
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (AccelerometerManager.isListening()) {
-            AccelerometerManager.stopListening();
-
-            //Toast.makeText(this, "onDestroy Accelerometer Stopped", Toast.LENGTH_SHORT).show();
+    /*private ShakeGestureManager.GestureListener mListener = new ShakeGestureManager.GestureListener() {
+        @Override
+        public void onGestureDetected(int gestureType, int gestureCount) {
+            // ジェスチャーを認識したらここが呼ばれる
+            System.out.println("認識");
+            count++;
         }
+        public void checkAnswer(View view) {
+            // answer?
+            Button clickedButton = (Button) view;
+            String clickedAnswer = clickedButton.getText().toString();
+        }
+            @Override
+        public void onMessage(String s) {
 
-        Log.e("MyActivity", "on Destroy");
-    }
+        }
+    };
+    */
+
+
 }
